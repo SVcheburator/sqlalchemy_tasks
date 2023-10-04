c6279dc58dfcdafde9a7421120928bfbf4f84664
Cheburator <sashadanilenko06@gmail.com>
2023-10-04 18:40:22 +0200

    fixed

--- hw_alchemy/src/db.py
+++ hw_alchemy/src/db.py
@@ -1,17 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import configparser
-import pathlib
 
-file_config = pathlib.Path(__file__).parent.parent.joinpath('config.ini')
-config = configparser.ConfigParser()
-config.read(file_config)
-
-username = config.get('DB', 'user')
-password = config.get('DB', 'password')
-db_name = config.get('DB', 'db_name')
-domain = config.get('DB', 'domain')
 
 url = 'sqlite:///hw_alchemy.db'
 Base = declarative_base()
